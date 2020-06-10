c2334a580f570b5866d5ab694c36b54c38185270
Lorenzo Meurs <lorenzomeurs@msn.com>
2020-06-10 22:29:15 +0200

    Draw result & create clear pattern

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 /* global window, document, Math */
 const STROKE_LENGTH = 100;
+const PATTERN_CLEAR = { angle1: 1, angle2: 1 / 180 };
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+const resultCanvas = document.getElementById('result-canvas');
+const resultContext = resultCanvas.getContext('2d');
 
 MagnetTable = {
   angle1: 0,
-  angle2: 0,
+  angle2: 0.1,
 };
 
 setInterval(() => {
   // Clear canvas
   context.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Normalize angle
+  MagnetTable.angle1 = MagnetTable.angle1 % 360;
+  MagnetTable.angle2 = MagnetTable.angle2 % 360;
 
   // Calculate positions
   const angle1 = MagnetTable.angle1 * Math.PI / 180;
@@ -30,6 +37,11 @@ setInterval(() => {
     y: STROKE_LENGTH * Math.sin(angle2 + angle1) + secondStrokeBegin.y,
   };
 
+  // Draw angles
+  context.font = '16px Roboto';
+  context.fillText(`angle 1: ${MagnetTable.angle1}`, 10, 26);
+  context.fillText(`angle 2: ${MagnetTable.angle2}`, 10, 52);
+
   // Draw first stroke
   context.strokeStyle = 'blue';
   context.beginPath();
@@ -38,11 +50,19 @@ setInterval(() => {
   context.stroke();
 
   // Draw second stroke
-  context.strokeStyle = 'red';
+  context.strokeStyle = 'darkblue';
   context.beginPath();
   context.moveTo(secondStrokeBegin.x, secondStrokeBegin.y);
   context.lineTo(secondStrokeEnd.x, secondStrokeEnd.y);
   context.stroke();
-}, 100);
+
+  // Draw result
+  resultContext.fillStyle = 'red';
+  resultContext.fillRect(secondStrokeEnd.x - 1, secondStrokeEnd.y - 1, 2, 2);
+
+  // Draw pattern
+  MagnetTable.angle1 += PATTERN_CLEAR.angle1;
+  MagnetTable.angle2 += PATTERN_CLEAR.angle2;
+}, 1);
 
 window.MagnetTable = MagnetTable;
